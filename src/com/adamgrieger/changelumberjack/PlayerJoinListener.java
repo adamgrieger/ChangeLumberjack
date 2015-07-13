dbde39047e6f0747510d1d5eb4bf039d37f153a2
Adam Grieger <adamgrieger@gmail.com>
2015-07-12 23:10:08 -0700

    Added ChangeLumberjack instance field

--- src/com/adamgrieger/changelumberjack/PlayerJoinListener.java
+++ src/com/adamgrieger/changelumberjack/PlayerJoinListener.java
@@ -5,25 +5,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+
 /**
- * Created by Adam on 7/10/2015.
+ * Class for performing actions when a players joins the server.
  */
 public class PlayerJoinListener implements Listener {
+
+    private final ChangeLumberjack plugin;
+
+    public PlayerJoinListener(ChangeLumberjack plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player joinedPlayer = event.getPlayer();
         boolean newPlayer = true;
 
-        for (int i = 0; i < ChangeLumberjack.lumberjackers.size() && newPlayer; i++) {
-            if (ChangeLumberjack.lumberjackers.get(i).getPlayer().getName().equals(joinedPlayer.getName())) {
+        for (int i = 0; i < plugin.lumberjackers.size() && newPlayer; i++) {
+            if (plugin.lumberjackers.get(i).getPlayer().getName().equals(joinedPlayer.getName())) {
                 newPlayer = false;
-                ChangeLumberjack.lumberjackers.get(i).showUnreadChanges();
+                plugin.lumberjackers.get(i).showUnreadChanges();
             }
         }
 
         if (newPlayer) {
-            ChangeLumberjack.addNewPlayer(joinedPlayer);
+            plugin.addNewPlayer(joinedPlayer);
         }
     }
 }
